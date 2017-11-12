cceffbaad23aea8781d4d3afcac4f26af9f7a298
Favio Tejada <favio_tejada@hotmail.com>
2017-11-12 21:14:03 +0100

    Add structural pattern: decorator

---
+++ src/dessignPatterns/Structural/decorator/DecoratorDemo.java
@@ -0,0 +1,19 @@
+package dessignPatterns.Structural.decorator;
+
+/**
+ * Advantages:
+ * Contains another entity -> composition
+ * Modifies behavior
+ * Does not change underlying object
+ *
+ * Pitfalls:
+ * Multiple specialized objects
+ * Confused with simple inheritance
+ */
+public class DecoratorDemo {
+
+    public static void main(String[] args) {
+        Sandwich sandwich = new DressingDecorator(new MeatDecorator(new SimpleSandwich()));
+        System.out.println(sandwich.make());
+    }
+}
---
+++ src/dessignPatterns/Structural/decorator/Sandwich.java
@@ -0,0 +1,8 @@
+package dessignPatterns.Structural.decorator;
+
+/**
+ * Created by Favio on 12/11/2017.
+ */
+public interface Sandwich {
+    public String make();
+}
