7d479b4799a6e475345ac41423205e3ca2532f51
Favio Tejada <favio_tejada@hotmail.com>
2017-11-12 21:13:11 +0100

    Add structural pattern: Adapter

---
+++ src/dessignPatterns/Structural/adapter/EmployeeClient.java
@@ -0,0 +1,29 @@
+package dessignPatterns.Structural.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Advantages:
+ * Deals with legacy code
+ * Pitfalls:
+ * Does not add extra functionality, otherwise it becomes decorator
+ */
+public class EmployeeClient {
+    private List<Employee> employeeList;
+
+    public List<Employee> getEmployeeList() {
+        employeeList = new ArrayList<>();
+        Employee employeeFromDB = new EmployeeDB("1", "Favio", "Tejada", "favio@gmail.com");
+        employeeList.add(employeeFromDB);
+
+        EmployeeLDAP employeeLDAP = new EmployeeLDAP("2", "Peter", "pan", "asd@gmail.com");
+        employeeList.add(new EmployeeAdapterLdap(employeeLDAP));
+
+
+        EmployeeCSV employeeCSV = new EmployeeCSV(1, "asd", "asd", "123");
+        employeeList.add(new EmployeeAdapterCSV(employeeCSV));
+
+        return employeeList;
+    }
+}
