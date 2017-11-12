aa657bdb1ba02d97b4962757f2c1bffe593c68b6
Favio Tejada <favio_tejada@hotmail.com>
2017-11-12 21:14:46 +0100

    Add structural pattern: Flyweight

---
+++ src/dessignPatterns/Structural/flyweight/Catalog.java
@@ -0,0 +1,24 @@
+package dessignPatterns.Structural.flyweight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Catalog acts as a factory and cache for Item flyweight objects
+ */
+public class Catalog {
+
+    private Map<String, Item> items = new HashMap<>();
+
+    // Factory method
+    public Item lookup(String itemName) {
+        if (!items.containsKey(itemName)) {
+            items.put(itemName, new Item(itemName));
+        }
+        return items.get(itemName);
+    }
+
+    public int totalItemsMade() {
+        return items.size();
+    }
+}
