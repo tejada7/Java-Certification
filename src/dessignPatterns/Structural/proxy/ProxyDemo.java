296fec08fcd93ed5d9ea49873dc19bb8ec794a10
Favio Tejada <favio_tejada@hotmail.com>
2017-11-12 21:15:00 +0100

    Add structural pattern: Proxy

---
+++ src/dessignPatterns/Structural/proxy/ProxyDemo.java
@@ -0,0 +1,17 @@
+package dessignPatterns.Structural.proxy;
+
+/**
+ * Advantages:
+ * Its main purpose is to control invocations at compile time
+ * Pitfalls:
+ * Only one proxy per object
+ * Adds another abstraction layer
+ */
+public class ProxyDemo {
+
+    public static void main(String[] args) {
+        TwitterService service = (TwitterService) SecurityProxy.newInstance(new TwitterServiceStub());
+        System.out.println(service.getTimeline("screenName"));
+        service.postToTimeline("screenName", "message");
+    }
+}
