8f1d11d5a196ff4fb993323480f22048003c216b
Favio Tejada <favio_tejada@hotmail.com>
2017-11-12 21:09:30 +0100

    Add creational pattern: Factory

---
+++ src/dessignPatterns/creational/factory/Page.java
@@ -0,0 +1,8 @@
+package dessignPatterns.creational.factory;
+
+/**
+ * Created by Favio on 11/11/2017.
+ */
+public abstract class Page {
+
+}
---
+++ src/dessignPatterns/creational/factory/PostPage.java
@@ -0,0 +1,7 @@
+package dessignPatterns.creational.factory;
+
+/**
+ * Created by Favio on 11/11/2017.
+ */
+public class PostPage extends Page {
+}
