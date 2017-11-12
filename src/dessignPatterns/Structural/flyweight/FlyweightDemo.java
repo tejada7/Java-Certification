0b4e0cb29b0b9862ba017b74f526f994c861fdd9
Favio Tejada <favio_tejada@hotmail.com>
2017-11-12 21:14:46 +0100

    Add structural pattern: Flyweight

---
+++ src/dessignPatterns/Structural/flyweight/FlyweightDemo.java
@@ -0,0 +1,32 @@
+package dessignPatterns.Structural.flyweight;
+
+/**
+ * Advantages:
+ * Focused on memory optimization
+ * Deals with immutable objects
+ * Pitfalls:
+ * Complex pattern, lots of pieces involved
+ * Patterns inside a pattern (factory embedded)
+ */
+public class FlyweightDemo {
+    public static void main(String[] args) {
+        InventorySystem ims = new InventorySystem();
+
+        ims.takeOrder("Roomba", 114);
+        ims.takeOrder("Bose écouteurs", 361);
+        ims.takeOrder("Samsung TV", 654);
+        ims.takeOrder("Bose écouteurs", 361);
+        ims.takeOrder("Roomba", 114);
+        ims.takeOrder("Iphone 6S", 584);
+        ims.takeOrder("Samsung TV", 654);
+        ims.takeOrder("Bose écouteurs", 361);
+        ims.takeOrder("Roomba", 114);
+        ims.takeOrder("Iphone 6S", 585);
+        ims.takeOrder("Bose écouteurs", 361);;
+        ims.takeOrder("Samsung TV", 654);
+
+        ims.process();
+
+        System.out.println(ims.report());
+    }
+}
