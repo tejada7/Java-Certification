145c31714020c081643881f0582815a0ca957325
Favio Tejada <favio_tejada@hotmail.com>
2017-11-12 21:13:11 +0100

    Add structural pattern: Adapter

---
+++ src/dessignPatterns/Structural/adapter/Employee.java
@@ -0,0 +1,11 @@
+package dessignPatterns.Structural.adapter;
+
+/**
+ * Created by Favio on 11/11/2017.
+ */
+public interface Employee {
+    public String getId();
+    public String getFirstName();
+    public String getLastName();
+    public String getEmail();
+}
