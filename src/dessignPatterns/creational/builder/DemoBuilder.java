ac41e8261c37bf423bc2619960f42f8bada12265
Favio Tejada <favio_tejada@hotmail.com>
2017-11-12 21:08:58 +0100

    Add creational pattern: Builder

---
+++ src/dessignPatterns/creational/builder/DemoBuilder.java
@@ -0,0 +1,21 @@
+package dessignPatterns.creational.builder;
+
+import java.time.LocalDate;
+
+/**
+ * Advantages:
+ * Flexibility in creation of object
+ *
+ * Pitfalls:
+ * It may seems code replication for each model class
+ * Immutable
+ * Complexity
+ * Inner static class
+ */
+public class DemoBuilder {
+    public static void main(String[] args) {
+        IdentityBean.IdentityBeanBuilder builder = new IdentityBean.IdentityBeanBuilder();
+        IdentityBean instance = builder.withId(1).withBirthDate(LocalDate.now()).build();
+        System.out.println(instance);
+    }
+}
