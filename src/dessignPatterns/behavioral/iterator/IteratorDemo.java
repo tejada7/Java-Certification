939da8ba9aea77ffc20c6c4670a7887902069a85
Favio Tejada <favio_tejada@hotmail.com>
2017-12-10 21:08:51 +0100

    Add behavioral pattern: Iterator

---
+++ src/dessignPatterns/behavioral/iterator/IteratorDemo.java
@@ -0,0 +1,34 @@
+package dessignPatterns.behavioral.iterator;
+
+import java.util.*;
+
+/**
+ * Advantages:
+ * Decouples data from algorithms to retrieve data
+ * Mainly used when changing Collection implementation e.g. List to Set
+ * Concurrent modification
+ *
+ * Pitfalls:
+ * No access to any index
+ * Unidirectional (incremental)
+ * Slower in some cases than compare with direct index access
+ */
+public class IteratorDemo {
+
+    public static void main(String[] args) {
+        BikeRepository repo = new BikeRepository();
+        repo.addBike("Cervelo");
+        repo.addBike("Scott");
+        repo.addBike("Fuji");
+
+        Iterator<String> bikeIteraror = repo.iterator();
+
+        while (bikeIteraror.hasNext()) {
+            System.out.println(bikeIteraror.next());
+        }
+
+        for (String bike: repo) {
+            System.out.println(bike);
+        }
+    }
+}
