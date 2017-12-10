07861c7ed7a4af969cc19d94fba66fbc51eef336
Favio Tejada <favio_tejada@hotmail.com>
2017-12-10 21:09:09 +0100

    Add behavioral pattern: Mediator

---
+++ src/dessignPatterns/behavioral/mediator/MediatorDemo.java
@@ -0,0 +1,33 @@
+package dessignPatterns.behavioral.mediator;
+
+/**
+ * This example works along with the command pattern
+ * Advantages:
+ * Define how objects interact with one another without having them refer to each other explicitly
+ * Loose coupling
+ * Reusable components
+ * References only to the mediator
+ * <p>
+ * Pitfalls:
+ * Deity object - everything to everybody
+ * Limits subclassing
+ * Concussing when to use it, specially against the command
+ */
+public class MediatorDemo {
+
+    public static void main(String[] args) {
+        Mediator mediator = new Mediator();
+        Light bedroomlight = new Light("Beedrom");
+        Light kitchenlight = new Light("Kitchen");
+
+        mediator.registerLight(bedroomlight);
+        mediator.registerLight(kitchenlight);
+        Command turnOnAllLightsCommand = new TurnOnAllLightsCommand(mediator);
+
+        turnOnAllLightsCommand.execute();
+
+        Command turnOffAllLightsCommand = new TurnOffAllLightsCommand(mediator);
+
+        turnOffAllLightsCommand.execute();
+    }
+}
---
+++ src/dessignPatterns/behavioral/mediator/TurnOffAllLightsCommand.java
@@ -0,0 +1,18 @@
+package dessignPatterns.behavioral.mediator;
+
+/**
+ * Created by Favio on 10/12/2017.
+ */
+public class TurnOffAllLightsCommand implements Command {
+
+    private Mediator mediator;
+
+    public TurnOffAllLightsCommand(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    @Override
+    public void execute() {
+        mediator.turnOffAllLights();
+    }
+}
