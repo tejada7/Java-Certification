a06e10ac7fc7d9aaa0ad2fb6ff425ec6ffa54a5f
Favio Tejada <favio_tejada@hotmail.com>
2017-12-17 22:56:00 +0100

    Add behavioral pattern: Observer

---
+++ src/dessignPatterns/behavioral/observer/Observer.java
@@ -0,0 +1,9 @@
+package dessignPatterns.behavioral.observer;
+
+/**
+ * Created by Favio on 17/12/2017.
+ */
+public abstract class Observer {
+    protected Subject subject;
+    abstract void update();
+}
