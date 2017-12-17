6c657b536cf9c33dd653b7c1e7d228d0d2489a94
Favio Tejada <favio_tejada@hotmail.com>
2017-12-17 22:55:42 +0100

    Add behavioral pattern: State

---
+++ src/dessignPatterns/behavioral/state/FanHighState.java
@@ -0,0 +1,20 @@
+package dessignPatterns.behavioral.state;
+
+public class FanHighState extends State {
+    private Fan fan;
+
+    public FanHighState(Fan fan) {
+        this.fan = fan;
+    }
+
+    @Override
+    public void handleRequest() {
+        System.out.println("Turning fan off.");
+        fan.setState(fan.getFanOffState());
+    }
+
+    @Override
+    public String toString() {
+        return "Fan is high.";
+    }
+}
---
+++ src/dessignPatterns/behavioral/state/FanMedState.java
@@ -0,0 +1,20 @@
+package dessignPatterns.behavioral.state;
+
+public class FanMedState extends State {
+    private Fan fan;
+
+    public FanMedState(Fan fan) {
+        this.fan = fan;
+    }
+
+    @Override
+    public void handleRequest() {
+        System.out.println("Turning fan on to high.");
+        fan.setState(fan.getFanHighState());
+    }
+
+    @Override
+    public String toString() {
+        return "Fan is med.";
+    }
+}
