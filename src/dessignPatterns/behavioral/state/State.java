9b33b5f8f86ed9035b3d8db23d6f8c03eb30b46c
Favio Tejada <favio_tejada@hotmail.com>
2017-12-17 22:55:42 +0100

    Add behavioral pattern: State

---
+++ src/dessignPatterns/behavioral/state/State.java
@@ -0,0 +1,8 @@
+package dessignPatterns.behavioral.state;
+
+public abstract class State {
+
+    public void handleRequest() {
+        System.out.println("Shouldn't be able to get here.");
+    }
+}
