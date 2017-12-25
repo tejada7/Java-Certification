0b4cd85e015238831e6f3e1542a0611ef64270cf
Favio Tejada <favio_tejada@hotmail.com>
2017-12-25 21:17:46 +0100

    Add behavioral pattern: Strategy

---
+++ src/dessignPatterns/behavioral/strategy/ValidationStrategy.java
@@ -0,0 +1,29 @@
+package dessignPatterns.behavioral.strategy;
+
+public abstract class ValidationStrategy {
+
+    public abstract boolean isValid(CreditCard creditCard);
+
+    /**
+     * Check whether the number is a legitimate creditcard number.
+     *
+     * @param ccNumber credit card number
+     * @return true if the number is a credit card number, false otherwise
+     */
+    protected boolean passesLuhn(String ccNumber) {
+        int sum = 0;
+        boolean alternate = false;
+        for (int i = ccNumber.length() - 1; i >= 0; i--) {
+            int n = Integer.parseInt(ccNumber.substring(i, i + 1));
+            if (alternate) {
+                n *= 2;
+                if (n > 9) {
+                    n = (n % 10) + 1;
+                }
+            }
+            sum += n;
+            alternate = !alternate;
+        }
+        return (sum % 10 == 0);
+    }
+}
