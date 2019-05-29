84cf4e233d9c8cc59dfd1cb569b0c68ff57f9a24
Favio Tejada <favio_tejada@hotmail.com>
2019-05-29 20:31:16 +0200

    Add DNA repeated sequences

---
+++ src/test/java/com/oca/interviewquestions/DNAUtilsTest.java
@@ -0,0 +1,43 @@
+package com.oca.interviewquestions;
+
+import org.junit.Test;
+
+import java.util.Collection;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class DNAUtilsTest {
+
+
+    @Test
+    public void noRepeatedSequences() {
+        String dna = "CTAATTCGACTATAGCGCTA";
+        Collection<String> result = DNAUtils.repeatedDna(dna);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void singleRepeatedSequence() {
+        String dna = "CTAATTCGACTATAGCGCTACTAATTCGAC";
+        Collection<String> result = DNAUtils.repeatedDna(dna);
+        assertTrue(result.contains("CTAATTCGAC"));
+    }
+
+    @Test
+    public void twoRepeatedSequences() {
+        String dna = "CTAATTCGACTATAGCGCTACTAATTCGACGTATAGCGCTA";
+        Collection<String> result = DNAUtils.repeatedDna(dna);
+        assertTrue(result.contains("CTAATTCGAC"));
+        assertTrue(result.contains("TATAGCGCTA"));
+    }
+
+    @Test
+    public void manyRepeatedOverlappingSequences() {
+        String dna =
+                "TATAGCGCTAATTCGACGACTATAGCGCTACGACGACTATCTAATTCGAC";
+        Collection<String> result = DNAUtils.repeatedDna(dna);
+        assertTrue(result.contains("TATAGCGCTA"));
+        assertTrue(result.contains("CTAATTCGAC"));
+        assertTrue(result.contains("CGACGACTAT"));
+    }
+}
