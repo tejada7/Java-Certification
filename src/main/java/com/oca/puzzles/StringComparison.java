412b2c9a4b6c506101d69557ebc60d941b0b48ba
Favio Tejada <favio_tejada@hotmail.com>
2020-07-26 01:26:53 +0200

    [Java puzzles] Beware of String == comparator

---
+++ src/main/java/com/oca/puzzles/StringComparison.java
@@ -0,0 +1,11 @@
+package com.oca.puzzles;
+
+public class StringComparison {
+    public static void main(String[] args) {
+        String s1 = "Hola";
+        String s2 = "Ho";
+        String s3 = "la";
+        System.out.println(s1 == s2 + s3);
+        System.out.println(s1 == s2.concat(s3).intern());
+    }
+}
