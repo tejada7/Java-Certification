626f153cf5696bba750deef00a78a62d8cace904
Favio Tejada <favio_tejada@hotmail.com>
2020-10-04 11:07:06 +0200

    Add reference method tricky inference example

---
+++ src/main/java/com/javacertification/ocp/ReferenceMethods.java
@@ -0,0 +1,22 @@
+package com.javacertification.ocp;
+
+import java.util.function.BiPredicate;
+
+public class ReferenceMethods {
+
+    public static void main(String[] args) {
+        // This is kind of tricky:
+        BiPredicate<String, String> predicate = String::startsWith;
+        // In fact, as this is a method reference at instance level and the BiPredicate takes two arguments,
+        // from which the first one corresponds to the string instance to where the predicate test is applied, the
+        // second corresponds to the startsWith argument of the method public boolean startsWith(String prefix)
+        // The point here is that Java infers which Predicate argument applies where based on the type.
+
+        // In order to test:
+        String originalString = "value";
+        assert !predicate.test(originalString, "V");
+
+        // and
+        assert predicate.test(originalString, "v");
+    }
+}
