c49300a2dac51b07d7786042535cf0dc3ff4ebe6
Favio Tejada <favio_tejada@hotmail.com>
2021-07-29 22:35:50 +0200

    Add complex reference method example

--- src/main/java/com/javacertification/ocp/ReferenceMethods.java
+++ src/main/java/com/javacertification/ocp/ReferenceMethods.java
@@ -1,5 +1,6 @@
 package com.javacertification.ocp;
 
+import java.util.List;
 import java.util.function.BiPredicate;
 
 public class ReferenceMethods {
@@ -18,5 +19,50 @@ public class ReferenceMethods {
 
         // and
         assert predicate.test(originalString, "v");
+    }
+
+    /**
+     * COMPLEX EXAMPLE:
+     */
+    interface Carnivore {
+        default int calories(List<String> foods) {
+            return foods.size() * 100;
+        }
+
+        int eat(List<String> foods);
+    }
+
+    static class Tiger implements Carnivore {
+        @Override
+        public int eat(List<String> foods) {
+            return foods.size();
+        }
+    }
+
+    static class TestClass {
+
+        public static int size(List<String> names) {
+            return names.size();
+        }
+
+        public int calculate(List<String> names) {
+            return 0;
+        }
+
+        public static void process(List<String> names, Carnivore carnivore) {
+            carnivore.eat(names);
+        }
+
+        public static void main(String[] args) {
+            var names = List.of("a", "b", "c");
+            var tiger = new Tiger();
+            var testClass = new TestClass();
+
+            process(names, tiger::eat);
+//            process(names, Tiger::eat); // This does not compile due to a missing reference to Tiger instance (as line above)
+            process(names, tiger::calories);
+            process(names, TestClass::size);
+            process(names, testClass::calculate);
+        }
     }
 }
