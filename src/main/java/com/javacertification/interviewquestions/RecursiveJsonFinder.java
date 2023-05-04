0108989ba65ff77930822bc63ffd09ab0d34af72
Favio Tejada <favio_tejada@hotmail.com>
2023-05-04 20:44:59 +0200

    Recursive Json Finder Final version

--- src/main/java/com/javacertification/interviewquestions/RecursiveJsonFinder.java
+++ src/main/java/com/javacertification/interviewquestions/RecursiveJsonFinder.java
@@ -2,25 +2,37 @@ package com.javacertification.interviewquestions;
 
 import org.json.JSONObject;
 
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.function.BiFunction;
 
-public class RecursiveJsonFinder implements BiFunction<String, String, Object> {
+public final class RecursiveJsonFinder implements BiFunction<String, String, Optional<Object>> {
 
     @Override
-    public Object apply(final String input, final String pathToFind) {
+    public Optional<Object> apply(final String input, final String pathToFind) {
         final var json = new JSONObject(input);
         final var paths = pathToFind.split("\\.");
         return findPathInJson(json, paths, 0);
     }
 
-    private Object findPathInJson(final JSONObject json, final String[] paths, int index) {
-        if (isLastIndex(paths, index) && json.get(paths[index]) != null) {
-            return json.get(paths[index]);
+    private Optional<Object> findPathInJson(final JSONObject json, final String[] paths, final int index) {
+        final var node = paths[index];
+        if (doesJsonContainNode(json, node)) {
+            return switch (json.get(node)) {
+                case JSONObject j -> !isLastIndex(paths, index) && doesJsonContainNode(j, paths[index + 1])
+                        ? findPathInJson(j, paths, index + 1)
+                        : Optional.of(j.toString());
+                case Object o -> Optional.of(o);
+            };
         }
-        return findPathInJson(json.getJSONObject(paths[index]), paths, ++index);
+        return Optional.empty();
     }
 
     private boolean isLastIndex(final String[] paths, final int index) {
         return paths.length - 1 == index;
+    }
+
+    private static boolean doesJsonContainNode(final JSONObject json, final String path) {
+        return new HashSet<>(json.names().toList()).contains(path);
     }
 }
--- src/test/java/com/javacertification/interviewquestions/RecursiveJsonFinderTest.java
+++ src/test/java/com/javacertification/interviewquestions/RecursiveJsonFinderTest.java
@@ -1,32 +1,84 @@
 package com.javacertification.interviewquestions;
 
-import org.assertj.core.api.BDDAssertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.assertj.core.api.BDDAssertions.*;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.BDDAssertions.then;
 
 @DisplayNameGeneration(ReplaceUnderscores.class)
 class RecursiveJsonFinderTest {
 
+    private final RecursiveJsonFinder recursiveJsonFinder = new RecursiveJsonFinder();
+
+    public static Stream<Arguments> fixture() {
+        return Stream.of(
+                Arguments.of("""
+                        {
+                            "foo": "1",
+                            "foo1": {
+                                "foo2": "2"
+                            }
+                        }
+                        """, "foo1.foo2", "2"),
+                Arguments.of(
+                        """
+                                {
+                                    "foo": "1",
+                                    "foo1": {
+                                        "foo2": "2"
+                                    },
+                                    "foo3": "3"
+                                }
+                                """, "foo3", "3"),
+                Arguments.of(
+                        """
+                                {
+                                    "foo": "1",
+                                    "foo1": {
+                                        "foo2": "2"
+                                    },
+                                    "foo3": "3"
+                                }
+                                """, "foo1", """
+                                {\
+                                "foo2":"2"\
+                                }\
+                                """
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("fixture")
+    void should_find_existing_path_from_json(final String input, final String pathToFind, final String expected) {
+        then(recursiveJsonFinder.apply(input, pathToFind)).contains(expected);
+    }
+
     @Test
-    void should_find_existing_path_from_json() {
+    void should_return_empty_when_path_not_found() {
         // Given
         final var input = """
                 {
                     "foo": "1",
                     "foo1": {
                         "foo2": "2"
-                    }
+                    },
+                    "foo3": "3"
                 }
                 """;
-        final var pathToFind = "foo1.foo2";
+        final var pathToFind = "foo4";
 
         // When
-        final Object actual = new RecursiveJsonFinder().apply(input, pathToFind);
+        final var actual = recursiveJsonFinder.apply(input, pathToFind);
 
         // Then
-        then(actual).isEqualTo("2");
+        then(actual).isEmpty();
     }
 }
