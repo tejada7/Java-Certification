3ad4524dc7e0a894f3f403b0d625a7857280c87a
Favio Tejada <favio_tejada@hotmail.com>
2023-05-08 11:22:27 +0200

    Add fraction simplifier

--- src/main/java/com/javacertification/Fraction.java
+++ src/main/java/com/javacertification/Fraction.java
@@ -1,5 +1,10 @@
 package com.javacertification;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.function.BiFunction;
+import java.util.function.UnaryOperator;
+
 public record Fraction(int integer, Decimal decimal) {
 
     public static Fraction of(final int integer, final String decimalAsString) {
@@ -12,28 +17,43 @@ public record Fraction(int integer, Decimal decimal) {
         return of(0, decimalAsString);
     }
 
-    public Fraction add(final Fraction another) {
-        final var numerator1 = decimal.numerator;
-        final var numerator2 = another.decimal.numerator;
+    public static BiFunction<Fraction, Fraction, Fraction> add() {
+        return (first, second) -> {
+            final var numerator1 = first.decimal.numerator();
+            final var numerator2 = second.decimal.numerator();
 
-        final var denominator1 = decimal.denominator;
-        final var denominator2 = another.decimal.denominator();
-        final int commonDenominator = resolveCommonDenominator(denominator1, denominator2);
+            final var denominator1 = first.decimal.denominator();
+            final var denominator2 = second.decimal.denominator();
+            final int commonDenominator = resolveCommonDenominator(denominator1, denominator2);
 
-        final var partialSum1 = commonDenominator / denominator1 * numerator1;
-        final var partialSum2 = commonDenominator / denominator2 * numerator2;
+            final var partialSum1 = commonDenominator / denominator1 * numerator1;
+            final var partialSum2 = commonDenominator / denominator2 * numerator2;
 
-        final var finalNumerator = partialSum1 + partialSum2;
-        final var partialDecimal = new Decimal(finalNumerator, commonDenominator);
+            final var finalNumerator = partialSum1 + partialSum2;
+            final var partialDecimal = new Decimal(finalNumerator, commonDenominator);
 
-        return new Fraction(0, partialDecimal);
+            return new Fraction(0, partialDecimal);
+        };
     }
 
-    private int resolveCommonDenominator(final int denominator1, final int denominator2) {
+    private static int resolveCommonDenominator(final int denominator1, final int denominator2) {
         final var maxDenominator = Math.max(denominator1, denominator2);
         return maxDenominator % Math.min(denominator1, denominator2) == 0
                 ? maxDenominator
                 : denominator1 * denominator2;
+    }
+
+    public static UnaryOperator<Fraction> simplify() {
+        return fraction -> {
+            final var numerator = fraction.decimal().numerator();
+            final var denominator = fraction.decimal().denominator();
+            final var maxMinPair = Pair.of(Math.max(numerator, denominator), Math.min(numerator, denominator));
+            final var quotient = maxMinPair.getLeft() / maxMinPair.getRight();
+            final var remainder = maxMinPair.getLeft() % maxMinPair.getRight();
+            return quotient >= 1
+                    ? new Fraction(fraction.integer() + quotient, new Decimal(remainder, denominator))
+                    : fraction;
+        };
     }
 
     public record Decimal(int numerator, int denominator) {
--- src/test/java/com/javacertification/FractionAdderTest.java
+++ src/test/java/com/javacertification/FractionAdderTest.java
@@ -10,17 +10,18 @@ import static org.assertj.core.api.BDDAssertions.then;
 class FractionAdderTest {
 
     @Test
-    void should_add_two_fractions() {
+    void should_add_and_simplify_the_addition_of_two_fractions() {
         // Given
         final var first = Fraction.of("1/2");
         final var second = Fraction.of("3/4");
 
         // When
-        final Fraction actual = first.add(second);
+        final var actual = Fraction.add()
+                .andThen(Fraction.simplify())
+                .apply(first, second);
 
         // Then
-        // final Fraction expected = Fraction.of(1, "1/4");
-        final Fraction expected = Fraction.of(0, "5/4");
+        final Fraction expected = Fraction.of(1, "1/4");
         then(actual).isEqualTo(expected);
     }
 }
